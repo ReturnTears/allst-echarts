a2cabb30952a2c0776bbdcb4fbb4ba9b22d03f03
returntears <returntears@outlook.com>
2019-05-01 00:22:49 +0800

    数据可视化, echarts

---
+++ src/main/java/com/vunn/echarts/AllstEchartsApplication.java
@@ -0,0 +1,18 @@
+package com.vunn.echarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot项目的启动类
+ * @author June
+ *
+ */
+@SpringBootApplication
+public class AllstEchartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllstEchartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vunn/echarts/AllstEchartsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vunn.echarts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllstEchartsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
